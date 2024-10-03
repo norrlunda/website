cc030c8f58fc3917b0a77bf0530ed1a11a8645ee
Johan Krusing <johan.krusing@hotmail.com>
2024-10-03 19:18:31 +0200

    Add modal to location

--- src/js/site.js
+++ src/js/site.js
@@ -4,7 +4,8 @@ function initCaseGeoMap(){
     let gbg = document.getElementById('gbg');
     let gbgList = document.getElementById('gbg-list');
     let listGroups = document.querySelectorAll('.list-group');
-    let modalLabel = document.getElementById('jobModalLabel');
+    let modalContent = document.querySelector('.modal-content');
+    let scaffoldData = [];
 
     const map = L.map('map', {
         zoomControl: false,
@@ -29,6 +30,14 @@ function initCaseGeoMap(){
 
     // map.on('click', onMapClick);
 
+    fetch('./src/json/scaffold.json')
+    .then(response => response.json())
+    .then(data => {
+       scaffoldData = data; 
+       console.log(scaffoldData);
+    })
+    .catch(error => console.error(error));
+
     fetch('./src/json/marker.json')
     .then(response => response.json())
     .then(data => {
@@ -46,7 +55,94 @@ function initCaseGeoMap(){
                 .bindPopup(button.outerHTML);
 
             marker.on('popupopen', () => {
-                modalLabel.innerText = `${job.name}`;
+                modalContent.innerHTML = '';
+
+                let modalHeader = document.createElement('div');
+                modalHeader.classList.add('modal-header');
+                
+                let modalTitle = document.createElement('h1');
+                modalTitle.id = 'job-modal-title';
+                modalTitle.classList.add('modal-title', 'fs-5');
+                modalTitle.innerText = `${job.name}`;
+
+                let modalHeaderButton = document.createElement('button');
+                modalHeaderButton.type = 'button';
+                modalHeaderButton.classList.add('btn-close');
+                modalHeaderButton.setAttribute('data-bs-dismiss', 'modal');
+                modalHeaderButton.setAttribute('aria-label', 'Close');
+                
+                modalHeader.appendChild(modalTitle);
+                modalHeader.appendChild(modalHeaderButton);
+
+                let modalBody = document.createElement('div');
+                modalBody.classList.add('modal-body');
+
+                let table = document.createElement('table');
+                table.classList.add('table', 'table-hover');
+
+                let tableHeader = document.createElement('thead');
+                
+                let tableHeaderNumber = document.createElement('th');
+                tableHeaderNumber.innerText = 'Artikel';
+
+                let tableHeaderName = document.createElement('th');
+                tableHeaderName.innerText = 'Beskrivning';
+
+                let tableHeaderCount = document.createElement('th');
+                tableHeaderCount.innerText = 'Antal';
+
+                tableHeader.appendChild(tableHeaderNumber);
+                tableHeader.appendChild(tableHeaderName);
+                tableHeader.appendChild(tableHeaderCount);
+
+                table.appendChild(tableHeader);
+                
+                let tableBody = document.createElement('tbody');
+
+                let scaffold = scaffoldData.find(scaffold => scaffold.number === job.number);
+                if(scaffold){
+                    scaffold.parts.forEach(part => {
+                        let tableRow = document.createElement('tr');
+                        
+                        let partNumberData = document.createElement('td');
+                        partNumberData.innerText = '#' + part.number;
+
+                        let partNameData = document.createElement('td');
+                        partNameData.innerText = part.name;
+
+                        let partCountData = document.createElement('td');
+                        partCountData.innerText = part.count;
+
+                        tableRow.appendChild(partNumberData);
+                        tableRow.appendChild(partNameData);
+                        tableRow.appendChild(partCountData);
+
+                        tableBody.appendChild(tableRow);
+                    });
+                }
+                table.appendChild(tableBody);                
+                modalBody.appendChild(table);
+
+                let modalFooter = document.createElement('div');
+                modalFooter.classList.add('modal-footer');
+
+                let modalCloseButton = document.createElement('button');
+                modalCloseButton.type = 'button';
+                modalCloseButton.classList.add('btn', 'btn-secondary');
+                modalCloseButton.setAttribute('data-bs-dismiss', 'modal');
+                modalCloseButton.innerText = 'Close';
+
+                let modalSaveButton = document.createElement('button');
+                modalSaveButton.type = 'button';
+                modalSaveButton.classList.add('btn', 'btn-success');
+                modalSaveButton.innerText = 'Save';
+
+                modalFooter.appendChild(modalCloseButton);
+                modalFooter.appendChild(modalSaveButton);
+
+                modalContent.appendChild(modalHeader);
+                modalContent.appendChild(modalBody);
+                modalContent.appendChild(modalFooter);
             });
 
             let listitem = document.createElement('small');
@@ -55,6 +151,10 @@ function initCaseGeoMap(){
 
             listitem.addEventListener('mouseover', () => {
                 marker.openPopup();
+            });
+            listitem.addEventListener('click', () => {
+                let modal = new bootstrap.Modal(document.getElementById('jobModal'));
+                modal.show();
             });
 
             if(job.lat > 58){
